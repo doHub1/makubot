d3b1df6c84443c7c5f13542410cdcc87b32d5a38
whiz777 <whiz777@users.noreply.github.com>
2016-01-17 11:57:23 +0900

    Tokenをスクリプトから削除

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export HUBOT_SLACK_TOKEN=xoxb-18012967430-Nx25Gl6i6DnG1vREkFXUjSEg
+export HUBOT_SLACK_TOKEN=xoxb-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 forever start -c coffee makubot -a slack
